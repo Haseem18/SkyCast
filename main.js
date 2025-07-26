3c00ddc76fe61ca03de9d3b10b6d7ffb267a9e3d
Haseem_Darve <webhaseem@gmail.com>
2025-07-26 22:03:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form");
+const place = document.querySelector(".place");
+const placeCondition = document.querySelector(".place_condition"); 
+const temprature = document.querySelector(".temperature");
+const humidity = document.querySelector(".humidity");
+const speed = document.querySelector(".speed");
+
+const updateweather = (data) => {
+    console.log(data);
+    
+    place.innerHTML = `${data.location.name}, ${data.location.country}`;
+    placeCondition.src = data.current.condition.icon;
+    temprature.innerHTML = `Temperature: ${data.current.temp_c} °C`;
+    humidity.innerHTML = `Humidity: ${data.current.humidity} %`;
+    speed.innerHTML = `Wind Speed: ${data.current.wind_kph} km/h`;
+}
+
+const fetchData = (event) => {
+    event.preventDefault();
+
+    const data = new FormData(form);
+    const { place: city } = Object.fromEntries(data.entries());
+    
+    fetch(`https://api.weatherapi.com/v1/current.json?key=604b3f8e19e249d799f70411250401&q=${city}&aqi=yes`)
+    .then(response => response.json())
+    .then(data => updateweather(data))
+    .catch(error => console.log(error))
+
+    form.reset();
+}
+
+form.addEventListener("submit", fetchData);
